705330f94ce34886a0ff5807c6692dd233b8a825
Raviteja Somisetty <ravitejasomisetty@Ravitejas-MacBook-Pro.local>
2018-03-04 07:40:34 -0500

    Filter by ListContacts state

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-
+import escapeRegEx from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component{
 state = {
@@ -11,6 +12,11 @@ updateQuery = (query) => {
 	this.setState({query: query.trim()})
 }
 	render(){
+		let showingContacts = this.props.contacts
+		if(this.state.query){
+			const match = new RegExp(escapeRegEx(this.state.query),'i')
+			showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+		}
 		return (
 			<div className='list-contacts'>
 			<div className = 'list-contacts-top'>
@@ -23,7 +29,7 @@ updateQuery = (query) => {
 			</div>
 
 			<ol className='contact-list'>
-				{this.props.contacts.map(contact => 
+				{showingContacts.map(contact => 
 					<li key = {contact.id} className='contact-list-item'>
 						<div className='contact-avatar' style={{
 							backgroundImage: `url(${contact.avatarURL})`
