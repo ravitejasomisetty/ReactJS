3f8820e2b3e9c9801adcbb34f99f3f1597baeedd
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-08-30 19:52:45 -0400

    redux with react native

--- ReactNative-Fitness/components/AddEntry.js
+++ ReactNative-Fitness/components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import { Ionicons } from '@expo/vector-icons'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
 import DateHeader from './DateHeader'
 import TextButton from './TextButton';
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn({ onPress }) {
     return (
@@ -16,7 +18,7 @@ function SubmitBtn({ onPress }) {
     )
 }
 
-export default class AddEntry extends Component {
+export class AddEntry extends Component {
     state = {
         run: 0,
         swim: 0,
@@ -56,6 +58,10 @@ export default class AddEntry extends Component {
         const key = timeToString()
         const entry = this.state
 
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
+
         this.setState(() => ({
             run: 0,
             swim: 0,
@@ -69,6 +75,11 @@ export default class AddEntry extends Component {
 
     reset = () => {
         const key = timeToString()
+
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
+
         removeEntry(key)
     }
 
@@ -118,3 +129,13 @@ export default class AddEntry extends Component {
         )
     }
 }
+
+function mapStateToProps(state){
+    const key = timeToString()
+
+    return{
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
