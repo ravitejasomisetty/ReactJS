3361563a0bfd5a6dbe66806fcf2f8faf1802723f
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-08-30 18:11:08 -0400

    slider and stepper

---
+++ ReactNative-Fitness/components/UdaciStepper.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
+
+export default function UdaciStepper({ max, unit, step, value, onIncrement, onDecrement }) {
+    return (
+        <View>
+            <View>
+            <TouchableOpacity onPress={onDecrement}>
+                <FontAwesome name='minus' color={'black'} size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onIncrement}>
+                <FontAwesome name='plus' color={'black'} size={30} />
+            </TouchableOpacity>
+            </View>
+
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
+        </View>
+    )
+}
