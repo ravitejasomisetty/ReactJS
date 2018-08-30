28a9ba99e15c071c7e40895e0ba3cd0a2852b213
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-08-30 18:45:23 -0400

    Revert "Revert "slider and stepper""
    This reverts commit c5d551c7861638f8f598b0dfc4a793ecea41bca1.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,13 +1,115 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { Ionicons } from '@expo/vector-icons'
+import UdaciSlider from './UdaciSlider'
+import UdaciStepper from './UdaciStepper'
+import DateHeader from './DateHeader'
+import TextButton from './TextButton';
 
+function SubmitBtn({ onPress }) {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        swim: 0,
+        bike: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+        this.setState((state) => {
+            const count = state[metric] + step
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
+    }
+
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        this.setState(() => ({
+            run: 0,
+            swim: 0,
+            bike: 0,
+            sleep: 0,
+            eat: 0
+        }))
+    }
+
+    reset = () => {
+        const key = timeToString()
+    }
+
     render() {
+        const metaInfo = getMetricMetaInfo()
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                        name='ios-happy-outline'
+                        size={100}>
+                        <Text>You have already logged</Text>
+                    </Ionicons>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
+
         return (
             <View>
-               {getMetricMetaInfo('bike').getIcon()}
+                <DateHeader date={new Date().toLocaleString()} />
+                {Object.keys(metaInfo).map((key) => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+
+                            {type === 'slider'
+                                ? <UdaciSlider value={value}
+                                    onChange={(value) => this.slide(key, value)}
+                                    {...rest} />
+                                : <UdaciStepper value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest} />}
+                        </View>
+                    )
+                })}
+
+                <SubmitBtn onPress={this.submit} />
             </View>
         )
     }
