6d6a68aa2046d1ccfc4ca7c534857e5133267151
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-08-31 22:23:49 -0400

    styling and layout

--- ReactNative-Fitness/components/AddEntry.js
+++ ReactNative-Fitness/components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import { Ionicons } from '@expo/vector-icons'
 import UdaciSlider from './UdaciSlider'
@@ -9,11 +9,13 @@ import TextButton from './TextButton';
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
 
 function SubmitBtn({ onPress }) {
     return (
-        <TouchableOpacity onPress={onPress}>
-            <Text>SUBMIT</Text>
+        <TouchableOpacity style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmiBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>SUBMIT</Text>
         </TouchableOpacity>
     )
 }
@@ -88,13 +90,13 @@ export class AddEntry extends Component {
 
         if (this.props.alreadyLogged) {
             return (
-                <View>
+                <View style={styles.center}>
                     <Ionicons
-                        name='ios-happy-outline'
+                        name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
                         size={100}>
                         <Text>You have already logged</Text>
                     </Ionicons>
-                    <TextButton onPress={this.reset}>
+                    <TextButton style={{ padding: 10 }} onPress={this.reset}>
                         Reset
                     </TextButton>
                 </View>
@@ -102,14 +104,14 @@ export class AddEntry extends Component {
         }
 
         return (
-            <View>
+            <View style={styles.container}>
                 <DateHeader date={new Date().toLocaleString()} />
                 {Object.keys(metaInfo).map((key) => {
                     const { getIcon, type, ...rest } = metaInfo[key]
                     const value = this.state[key]
 
                     return (
-                        <View key={key}>
+                        <View key={key} style={styles.row}>
                             {getIcon()}
 
                             {type === 'slider'
@@ -130,10 +132,54 @@ export class AddEntry extends Component {
     }
 }
 
-function mapStateToProps(state){
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: white
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40
+    },
+    androidSubmiBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 2,
+        height: 45,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginLeft: 30
+    }
+})
+
+function mapStateToProps(state) {
     const key = timeToString()
 
-    return{
+    return {
         alreadyLogged: state[key] && typeof state[key].today === 'undefined'
     }
 }
