144b78a7aecf32403b3c71f69053837bf0f212b2
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2018-09-01 09:55:03 -0400

    TabNavigator between History and AddEntry components

--- ReactNative-Fitness/App.js
+++ ReactNative-Fitness/App.js
@@ -1,16 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import History from './components/History'
+import { createBottomTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+
+function CustomStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }} >
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = createBottomTabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+    tabBarOptions: {
+      activeTintColor: purple,
+      style: {
+        height: 56,
+        backgroundColor: white,
+        shadowRadius: 6,
+        shadowOpacity: 1,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        }
+      }
+    }
+  })
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <CustomStatusBar backgroundColor={purple} barStyle='light-content' />
+          <Tabs />
         </View>
       </Provider>
     )
